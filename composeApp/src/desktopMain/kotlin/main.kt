f8f75a2bb991a7611c941a7c7b7317c7d33518ab
Daniel Bälz <info@dbaelz.de>
2023-05-10 12:41:37 +0200

    Provide title string for desktop app from libres res

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -3,6 +3,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import de.dbaelz.compcardero.App
+import de.dbaelz.compcardero.MainRes
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
@@ -10,7 +11,7 @@ fun main() = application {
     Napier.base(DebugAntilog())
 
     Window(
-        title = "CompCardero",
+        title = MainRes.string.app_name,
         state = rememberWindowState(width = 800.dp, height = 600.dp),
         onCloseRequest = ::exitApplication,
     ) { App() }
