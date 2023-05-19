3089bb76415a575e95d1f3dd3617cc04b8509221
Daniel Bälz <info@dbaelz.de>
2023-05-19 11:41:12 +0200

    Rename files to fix issues with Android

--- composeApp/src/commonMain/kotlin/de/dbaelz/compcardero/decks/FantasyDeck.kt
+++ composeApp/src/commonMain/kotlin/de/dbaelz/compcardero/decks/FantasyDeck.kt
@@ -4,31 +4,31 @@ import de.dbaelz.compcardero.MR
 import de.dbaelz.compcardero.data.GameCard
 
 val fantasyCardDeck: MutableList<GameCard> = mutableListOf(
-    GameCard("orc-armored-1", 1, 0, 1, MR.images.`orc-armored-1`),
-    GameCard("orc-armored-1", 1, 0, 1, MR.images.`orc-armored-1`),
-    GameCard("orc-armored-1", 1, 0, 1, MR.images.`orc-armored-1`),
-    GameCard("orc-armored-1", 1, 0, 1, MR.images.`orc-armored-1`),
+    GameCard("orc_armored_1", 1, 0, 1, MR.images.orc_armored_1),
+    GameCard("orc_armored_1", 1, 0, 1, MR.images.orc_armored_1),
+    GameCard("orc_armored_1", 1, 0, 1, MR.images.orc_armored_1),
+    GameCard("orc_armored_1", 1, 0, 1, MR.images.orc_armored_1),
 
-    GameCard("human-male-armor", 2, 0, 2, MR.images.`human-male-armor`),
-    GameCard("human-male-armor", 2, 0, 2, MR.images.`human-male-armor`),
-    GameCard("human-male-armor", 2, 0, 2, MR.images.`human-male-armor`),
+    GameCard("human_male_armor", 2, 0, 2, MR.images.human_male_armor),
+    GameCard("human_male_armor", 2, 0, 2, MR.images.human_male_armor),
+    GameCard("human_male_armor", 2, 0, 2, MR.images.human_male_armor),
 
-    GameCard("gnome-male-mage-2", 1, 1, 2, MR.images.`gnome-male-mage-2`),
-    GameCard("gnome-male-mage-2", 1, 1, 2, MR.images.`gnome-male-mage-2`),
+    GameCard("gnome_male_mage_2", 1, 1, 2, MR.images.gnome_male_mage_2),
+    GameCard("gnome_male_mage_2", 1, 1, 2, MR.images.gnome_male_mage_2),
 
-    GameCard("human-female-leather", 2, 1, 3, MR.images.`human-female-leather`),
-    GameCard("human-female-leather", 2, 1, 3, MR.images.`human-female-leather`),
-    GameCard("orc-armored-2", 3, 0, 3, MR.images.`orc-armored-2`),
-    GameCard("gnome-female-robe", 0, 3, 3, MR.images.`gnome-female-robe`),
+    GameCard("human_female_leather", 2, 1, 3, MR.images.human_female_leather),
+    GameCard("human_female_leather", 2, 1, 3, MR.images.human_female_leather),
+    GameCard("orc_armored_2", 3, 0, 3, MR.images.orc_armored_2),
+    GameCard("gnome_female_robe", 0, 3, 3, MR.images.gnome_female_robe),
 
-    GameCard("orc-mage-1", 3, 1, 4, MR.images.`orc-mage-1`),
-    GameCard("gnome-male-mage-1", 4, 0, 4, MR.images.`gnome-male-mage-1`),
-    GameCard("orc-shaman", 2, 2, 4, MR.images.`orc-shaman`),
+    GameCard("orc_mage_1", 3, 1, 4, MR.images.orc_mage_1),
+    GameCard("gnome_male_mage_1", 4, 0, 4, MR.images.gnome_male_mage_1),
+    GameCard("orc_shaman", 2, 2, 4, MR.images.orc_shaman),
 
-    GameCard("human-male-mage", 3, 2, 5, MR.images.`human-male-mage`),
-    GameCard("human-female-mage", 5, 0, 5, MR.images.`human-female-mage`),
+    GameCard("human_male_mage", 3, 2, 5, MR.images.human_male_mage),
+    GameCard("human_female_mage", 5, 0, 5, MR.images.human_female_mage),
 
-    GameCard("orc-mage-2", 6, 0, 6, MR.images.`orc-mage-2`),
-    GameCard("gnome-female-armor", 5, 2, 6, MR.images.`gnome-female-armor`),
-    GameCard("gnome-female-armor", 5, 2, 6, MR.images.`gnome-female-armor`)
+    GameCard("orc_mage_2", 6, 0, 6, MR.images.orc_mage_2),
+    GameCard("gnome_female_armor", 5, 2, 6, MR.images.gnome_female_armor),
+    GameCard("gnome_female_armor", 5, 2, 6, MR.images.gnome_female_armor)
 )
