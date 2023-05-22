dc45606f884f03e8e720fa3f7af1f1d2d653547a
Daniel Bälz <info@dbaelz.de>
2023-05-22 10:54:02 +0200

    Adjust about screen and add link to project on GitHub

--- composeApp/src/commonMain/kotlin/de/dbaelz/compcardero/ui/about/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/de/dbaelz/compcardero/ui/about/AboutScreen.kt
@@ -1,12 +1,15 @@
 package de.dbaelz.compcardero.ui.about
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -23,8 +26,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.model.rememberScreenModel
@@ -73,6 +81,9 @@ private fun AboutContent(paddingValues: PaddingValues) {
             .fillMaxSize()
             .padding(paddingValues)
             .background(MaterialTheme.colors.secondary)
+            .padding(16.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .border(2.dp, MaterialTheme.colors.onSecondary, RoundedCornerShape(8.dp))
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
@@ -93,10 +104,25 @@ private fun AboutContent(paddingValues: PaddingValues) {
                 textAlign = TextAlign.Center
             )
 
-            Text(
-                text = stringResource(MR.strings.about_github_link),
-                fontSize = 18.sp,
-                textAlign = TextAlign.Center
+            val githubText = buildAnnotatedString {
+                append(stringResource(MR.strings.about_github_text))
+                addStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colors.onPrimary,
+                        fontSize = 18.sp,
+                        textDecoration = TextDecoration.Underline
+                    ),
+                    start = 0,
+                    end = this.length
+                )
+            }
+            val uriHandler = LocalUriHandler.current
+            val githubLink = stringResource(MR.strings.about_github_link)
+            ClickableText(
+                text = githubText,
+                onClick = {
+                    uriHandler.openUri(githubLink)
+                }
             )
         }
     }
