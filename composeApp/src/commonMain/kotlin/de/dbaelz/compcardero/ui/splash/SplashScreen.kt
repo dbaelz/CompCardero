c5c6edb9d5e0a89126a6b9b6bfefb725226cab17
Daniel Bälz <info@dbaelz.de>
2023-05-25 16:06:03 +0200

    Show card images on splash screen

--- composeApp/src/commonMain/kotlin/de/dbaelz/compcardero/ui/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/de/dbaelz/compcardero/ui/splash/SplashScreen.kt
@@ -1,10 +1,18 @@
 package de.dbaelz.compcardero.ui.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -12,8 +20,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -23,10 +34,12 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import de.dbaelz.compcardero.MR
-import de.dbaelz.compcardero.getPlatformName
+import de.dbaelz.compcardero.decks.fantasyGameDeck
 import de.dbaelz.compcardero.ui.mainmenu.MainMenuScreen
 import de.dbaelz.compcardero.ui.splash.SplashScreenContract.Event
 import de.dbaelz.compcardero.ui.splash.SplashScreenContract.Navigation
+import dev.icerock.moko.resources.ImageResource
+import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
 
 class SplashScreen : Screen {
@@ -52,7 +65,11 @@ private fun Content(sendEvent: (Event) -> Unit) {
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.primary)
-            .clickable { sendEvent(Event.ScreenClicked) },
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = { sendEvent(Event.ScreenClicked) }
+            ),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
     ) {
@@ -60,9 +77,9 @@ private fun Content(sendEvent: (Event) -> Unit) {
             LocalContentColor provides MaterialTheme.colors.onPrimary
         ) {
             Text(
-                text = "${stringResource(MR.strings.splash_headline)} ${getPlatformName()}",
+                text = stringResource(MR.strings.app_name),
                 fontWeight = FontWeight.ExtraBold,
-                fontSize = 20.sp,
+                fontSize = 32.sp,
                 textAlign = TextAlign.Center
             )
 
@@ -72,5 +89,40 @@ private fun Content(sendEvent: (Event) -> Unit) {
                 textAlign = TextAlign.Center
             )
         }
+
+        Spacer(Modifier.height(16.dp))
+
+        BoxWithConstraints {
+            val availableHeight = maxHeight / 2
+            val availableWidth = maxWidth / 2
+
+            val widthCard = maxOf(availableHeight.value * (2.0 / 3.0), 1.0)
+            val availableElements = maxOf((availableWidth.value / widthCard), 1.0).toInt()
+
+            val images by remember {
+                mutableStateOf(fantasyGameDeck.cards.shuffled().take(availableElements - 1))
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)
+            ) {
+                val modifier = Modifier.height(availableHeight)
+                CardTeaserImage(modifier, fantasyGameDeck.deckCard)
+
+                images.forEach {
+                    CardTeaserImage(modifier, it.imageResource)
+                }
+            }
+        }
     }
+}
+
+@Composable
+private fun CardTeaserImage(modifier: Modifier, imageResource: ImageResource) {
+    Image(
+        painter = painterResource(imageResource = imageResource),
+        contentDescription = null,
+        modifier = modifier.clip(RoundedCornerShape(8.dp))
+    )
 }
